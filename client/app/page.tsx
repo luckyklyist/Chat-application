8911da3455d156d4131848f4b7307d7bcc21dcf4
anupam <puspaanupam@gmail.com>
2023-06-21 19:35:35 +0545

    client added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Chat application configured !!</h2>
+      <h2>Chat application configured dssds!!</h2>
     </main>
   )
 }
